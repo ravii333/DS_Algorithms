03b5a6be5cecd96867d7b81ec47581ccb2d1b6cf
vira069 <vira.kumar069@gmail.com>
2024-01-25 07:55:49 +0530

    Some recursion practices

--- RotateMatrix.java
+++ RotateMatrix.java
@@ -11,30 +11,31 @@ public class RotateMatrix {
         }
     }
     public static void transposeInplace(int[][] matrix,int r,int c){
-        for(int i=0;i<c;i++){
-            for(int j=0;j<r;j++){
+        for(int i=0;i<r;i++){
+            for(int j=i+1;j<c;j++){
                 int temp = matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
     }
-    public static void reverseArray(int[][] arr){
-        int i=0;
-        int j=arr.length-1;
+    public static void reverseArray(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
 
-        while(i<j){
-            int temp=arr[i];
-            arr[i]= arr[j];
-            arr[j]=temp;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             i++;
             j--;
         }
     }
     public static void rotate(int[][] matrix){
+        int n = matrix.length;
         transposeInplace(matrix, n, n);
         for(int i=0;i<n;i++){
-            reverseArray(matrix[i][j]);
+            reverseArray(matrix[i]);
         }
     }
     
