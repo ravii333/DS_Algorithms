23f805933a0bf86b32bb20d3b491dd910bb65b42
vira069 <vira.kumar069@gmail.com>
2024-05-14 08:54:12 +0530

    completed the program

--- com/ravi/Binary_Search/FindTargetIndex.java
+++ com/ravi/Binary_Search/FindTargetIndex.java
@@ -1,34 +1,43 @@
 package com.ravi.Binary_Search;
 
+// FIND THE INDEX OF TARGET ELEMENT IN THE ARRAY
+
 public class FindTargetIndex {
-    static void displayArray(int[] array){
-        for(int ele : array){
-            System.out.print(ele+ " ");
+    static void displayArray(int[] array) {
+        for (int ele : array) {
+            System.out.print(ele + " ");
         }
     }
-    static int findIndex(int[] arr,int target){
-        int start =0;
-        int n = arr.length;
-        int end = n-1;
-        int idx = -1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(arr[mid]==target){
-                idx = mid;
-                return idx;
-            }
-            if(arr[mid]>target){
-                start = mid-1;
-            }
-            if(arr[mid]<target){
 
+    static int findIndex(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] <= arr[end]) {
+                if (target > arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (target >= arr[start] && target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
-
         }
+        return -1;
     }
+
     public static void main(String[] args) {
-        int[] arr = {3,4,5,1,2};
+        int[] arr = {3, 4, 5, 1, 2};
+        System.out.println("Given Array :");
         displayArray(arr);
-
+        int target = 4;
+        System.out.println("\nIndex of target element: " + findIndex(arr, target));
     }
 }
