7cbe5cc749743838ea02e256e275ed86361ee503
vira069 <vira.kumar069@gmail.com>
2024-08-29 21:02:55 +0530

    Some class files

--- com/ravi/LinkedList/Delete_Middile_Node.java
+++ com/ravi/LinkedList/Delete_Middile_Node.java
@@ -1,2 +1,27 @@
-package com.ravi.LinkedList;public class Delete_Middile_Node {
+package com.ravi.LinkedList;
+
+public class Delete_Middile_Node {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            data = this.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(4);
+        Node b = new Node(24);
+        Node c = new Node(8);
+        Node d = new Node(5);
+        Node e = new Node(7);
+        Node f = new Node(9);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+    }
 }
