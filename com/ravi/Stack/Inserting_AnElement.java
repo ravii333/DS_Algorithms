1b47385368cbe8f33dd4c2c3ec1f9e2339130021
vira069 <vira.kumar069@gmail.com>
2024-09-12 22:59:45 +0530

    Inserting An element in the stack

---
+++ com/ravi/Stack/Inserting_AnElement.java
@@ -0,0 +1,27 @@
+package com.ravi.Stack;
+
+import java.util.Stack;
+
+public class Inserting_AnElement {
+    public static void main(String[] args) {
+        Stack<Integer>st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        int idx = 2;
+        int x = 13;
+
+        Stack<Integer>temp = new Stack<>();
+        while(st.size()>idx){
+            temp.push(st.pop());
+        }
+        st.push(x);
+        while(!temp.isEmpty()){
+            st.push(temp.pop());
+        }
+        System.out.println(st);
+    }
+}
